6f31f29caf6da956a05dc673fe7d80e3296922a2
sarkariprep.in <114567151+sarkariprepofficial@users.noreply.github.com>
2022-09-28 17:46:26 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,363 @@
+<!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="Cache-control" content="no-cache" />
+<meta name="website" content="https://sarkariprep.in">
+<meta name="robots" content="NOODP, NOYDIR" />
+<meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+<meta name="copyright" content="sarkariprep.in">
+<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+<meta content='IE=9; IE=8; IE=7; IE=EDGE; chrome=1' http-equiv='X-UA-Compatible' />
+<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- search engine optimization by SRP - https://sarkariprep.in/ -->
+<title>Sarkariprep App And Sarkari Exam Result App Page</title>
+<link rel="canonical" href="https://sarkariprep.in/app/" />
+<link rel="icon" href="https://sarkariprep.in/images/sarkariprep-fevicon.png" />
+<link rel="apple-touch-icon" href="https://sarkariprep.in/images/sarkariprep-fevicon.png">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+ <style>
+ @font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  src: url(https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxK.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+ html, body {
+   font-family: 'Roboto';
+   overflow-x: hidden;
+}*{margin: 0;padding: 0;outline: none;border: none;-webkit-box-sizing: border-box;}*:before,*:after{-webkit-box-sizing: border-box;}
+body a{
+    color:#50B167;
+    text-decoration:none;
+}
+
+:root {
+        --shadow-1: 0 7px 5px -6px rgba(150,170,180,0.5); /*Header & Sidenav Close*/
+        --shadow-2: 8px 0 10px -8px rgba(150,170,180,0.5); /*Sidenav Right Side*/
+        --shadow-3: rgba(100, 100, 111, 0.5) 0px 3px 9px 0px; /*Social Share Button*/
+        --shadow-4: 0 -8px 10px -7px rgba(150,170,180,0.5); /*Mobile Footer*/
+    }
+
+.header{
+        background-color:#FFF;
+        color:#FFF;
+        text-align:center;
+        font-family:'Roboto';
+        font-style: normal;
+        font-size:25px;
+        font-weight: bold;
+        padding-top:12px;
+        height:60px;
+        border-bottom:2px solid #50B167;
+        box-shadow: var(--shadow-1);
+    }
+
+.menuicon {
+        display: block;
+        float: left;
+        position: absolute;
+        top:8px;
+        left:10px;
+    }
+
+.sidenav {
+      height: 100%;
+      width: 0;
+      position: fixed;
+      z-index: 1;
+      top: 0;
+      left: 0;
+      background-color: #FFF;
+      color:000000;
+      overflow-x: hidden;
+      transition: 0.5s;
+      padding-top: 47px;
+      box-shadow: var(--shadow-2);
+    }
+
+.sideitem a {
+      padding: 8px 8px 8px 15px;
+      text-decoration: none;
+      font-size: 16px;
+      font-weight:bold;
+      color: #000000;
+      display: block;
+      border-bottom:1px solid #DDDDDD;
+    }
+
+.sideitem a:hover {
+      background-color:#50B167;
+      color: #FFF;
+    }
+
+.sidenav .closebtn {
+      position: absolute;
+      top: 0;
+      padding-left:20px;
+      padding-top:15px;
+      font-size: 25px;
+      background-color:#50B167;
+      color:#FFF;
+      width: 290px;
+      height: 60px;
+      box-shadow: var(--shadow-1);
+    }
+
+.sideitem {
+        margin-top:13px;
+    }
+.sideitem li{
+        line-height:1.8;
+    }
+
+/*-----------------------
+Drop Down Menu
+------------------------*/
+
+#mySidenav::-webkit-scrollbar{
+	    width: 0px;
+    }
+
+input[type="checkbox"]{
+        display: none;
+    }
+.srp-menu ul,
+.srp-menu ul ul{
+        position: static;
+        opacity: 1;
+        visibility: visible;
+        background: none;
+        max-height: 0px;
+        overflow: hidden;
+        background: #50B167;
+        color:#fff;
+    }
+.drop-down a{
+      color:#fff;
+    }
+.drop-down a:hover{
+      color:#fff;
+    }
+.srp-menu #education:checked ~ ul,
+.srp-menu #exam:checked ~ ul,
+.srp-menu #languages:checked ~ ul,
+.srp-menu #me-details:checked ~ ul,
+.srp-menu #top-jobs:checked ~ ul,
+.srp-menu #test-series:checked ~ ul{
+        max-height: 100vh;
+    }
+.srp-menu ul li{
+        margin: 0px 0px;
+    }
+label {
+      padding: 8px 8px 8px 15px;
+      text-decoration: none;
+      font-size: 16px;
+      font-weight:bold;
+      color: #000000;
+      display: block;
+      border-bottom:1px solid #DDDDDD;
+    }
+.drop-down-icon::after {
+      float: right;
+      margin-right:10px;
+      content: "+";
+    }
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 12px;}
+}
+
+
+.main-data {
+    width:100%;
+    height:auto;
+}
+
+.app p{
+    font-size:20px;
+    color:#000;
+    text-align:center;
+    padding:10px;
+    font-weight:bold;
+}
+
+.chip {
+  display: inline-block;
+  padding: 5px;
+  padding-right:10px;
+  line-height: 40px;
+  border-radius: 100px;
+  background-color: #50B167;
+  color:#FFF;
+  font-weight:bold;
+  font-size:18px;
+  margin:20px;
+  width:200px;
+}
+
+.chip img {
+  float: left;
+  margin-right:5px;
+  height: 40px;
+  width: 40px;
+}
+
+
+.footer {
+    background-color:#181818; 
+    color:#FFF; 
+    font-size:12px;
+    line-height:1.5;
+    padding:10px;
+    text-align:center;
+    width:100%;
+}
+
+.footer a{
+    color:#50B167;
+}
+
+@media only screen and (min-width: 1200px)  {
+
+.fixfooter {
+    display:none;
+}
+  
+}
+</style>
+<header>
+  <nav>
+    <div class="header">
+      <div class="menuicon">
+        <p style="font-size:30px;cursor:pointer; color:#50B167; align:left;" onclick="openNav()">&#9776;</p></div>
+         <!--Logo-->
+          <a href="https://sarkariprep.in/"><img src="https://sarkariprep.in/images/sarkariprep.png" width="220px" height="35px" alt="sarkariprep"></a> </div>
+            </nav>
+            <!--Sidenav-->
+            <nav>
+                <div id="mySidenav" class="sidenav">
+                    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a> 
+                    <div class="sideitem">
+                        <ul class="srp-menu">
+                            <!-- 1 Drop Down -->
+                            <li>
+                                <input type="checkbox" id="top-jobs">
+                                <label for="top-jobs"><span class="drop-down-icon">टॉप जॉब्स</span></label>
+                                <ul class="drop-down">
+                                    <li><a href="https://sarkariprep.in/govt-jobs/">सरकारी नौकरी</a></li>
+                                    <li><a href="https://sarkariprep.in/govt-jobs/railway-jobs/">रेलवे जॉब</a></li>
+                                    <li><a href="https://sarkariprep.in/govt-jobs/bank-jobs/">बैंक जॉब</a></li>
+                                    <li><a href="https://sarkariprep.in/govt-jobs/police-job-notification/">पुलिस जॉब</a></li>
+                                    <li><a href="https://sarkariprep.in/govt-jobs/defence-job/">डिफेंस जॉब</a></li>
+                                    <li><a href="https://sarkariprep.in/govt-jobs/teaching-jobs/">टीचिंग जॉब</a></li>
+                                    <li><a href="https://sarkariprep.in/govt-jobs/upcoming-govt-jobs/">अपकमिंग जॉब</a></li>
+                                </ul>
+                            </li>
+                            <!-- 2 Drop Down -->
+                            <li>
+                                <input type="checkbox" id="exam">
+                                <label for="exam"><span class="drop-down-icon">एग्जाम</span></label>
+                                <ul class="drop-down">
+                                    <li><a href="https://sarkariprep.in/govt-jobs/ssc-jobs/">एसएससी</a></li>
+                                    <li><a href="https://sarkariprep.in/psc-jobs/">पीएससी</a></li>
+                                    <li><a href="https://sarkariprep.in/upsc-jobs/">यूपीएससी</a></li>
+                                    <li><a href="https://sarkariprep.in/board-exam/">बोर्ड परीक्षा</a></li>
+                                </ul>
+                            </li>
+                            <!-- 2 Drop Down End -->
+                            <li><a href="https://sarkariprep.in/state-wise-government-jobs/">राज्य</a></li>
+                            <!--3 Drop Down-->
+                            <li>
+                                <input type="checkbox" id="education">
+                                <label for="education"><span class="drop-down-icon">योग्यता</span></label>
+                                <ul class="drop-down">
+                                    <li><a href="https://sarkariprep.in/5th-pass-sarkari-naukri/">पांचवी पास</a></li>
+                                    <li><a href="https://sarkariprep.in/8th-pass-sarkari-naukri/">आठवीं पास</a></li>
+                                    <li><a href="https://sarkariprep.in/10th-pass-sarkari-naukri/">दसवीं पास</a></li>
+                                    <li><a href="https://sarkariprep.in/12th-pass-sarkari-naukri/">बारहवीं पास</a></li>
+                                    <li><a href="https://sarkariprep.in/iti-pass-sarkari-naukri/">आईटीआई</a></li>
+                                    <li><a href="https://sarkariprep.in/diploma-sarkari-naukri/">डिप्लोमा</a></li>
+                                    <li><a href="https://sarkariprep.in/graduate-jobs/">ग्रेजुएट पास</a></li>
+                                </ul>
+                            </li>
+                            <!-- 3 Drop Down End-->
+                            <li><a href="https://sarkariprep.in/admit-card/">एडमिट कार्ड</a></li>
+                            <li><a href="https://sarkariprep.in/syllabus/">सिलेबस</a></li>
+                            <li><a href="https://sarkariprep.in/sarkariresult/">रिजल्ट</a></li>
+                            <li><a href="https://sarkariprep.in/sarkariyojana/">योजना</a></li>
+                            <li><a href="https://sarkariprep.in/e-services/">ई-सर्विस</a></li>
+                            <!-- 4 Drop Down -->
+                            <li>
+                                <input type="checkbox" id="test-series">
+                                <label for="test-series"><span class="drop-down-icon">टेस्ट सीरीज</span></label>
+                                <ul class="drop-down">
+                                    <li><a href="https://sarkariprep.in/online-test-series/">फ्री टेस्ट सीरीज</a></li>
+                                    <li><a href="https://sarkariprep.in/online-test-series/gk/">सामान्य ज्ञान</a></li>
+                                    <li><a href="https://sarkariprep.in/online-test-series/practice-set/">प्रैक्टिस सेट</a></li>
+                                    <li><a href="https://sarkariprep.in/online-test-series/quiz-test/">क्वीज टेस्ट</a></li>
+                                    <li><a href="https://sarkariprep.in/current-affairs/">करंट अफेयर्स</a></li>
+                                </ul>
+                            </li>
+                            <!-- 5 Drop Down -->
+                            <li>
+                                <input type="checkbox" id="languages">
+                                <label for="languages"><span class="drop-down-icon">भाषा</span></label>
+                                <ul class="drop-down">
+                                    <li><a href="https://sarkariprep.in/">हिंदी</a></li>
+                                    <li><a href="https://sarkariprep.in/en/">अंग्रेजी</a></li>
+                                </ul>
+                            </li>
+                            <!-- 6 Drop Down -->
+                            <li>
+                                <input type="checkbox" id="me-details">
+                                <label for="me-details"><span class="drop-down-icon">अन्य</span></label>
+                                <ul class="drop-down">
+                                    <li><a href="https://sarkariprep.in/about/">हमारे बारे में</a></li>
+                                    <li><a href="https://sarkariprep.in/contact/">संपर्क</a></li>
+                                    <li><a href="https://sarkariprep.in/privacy-policy/">प्राइवेसी पॉलिसी</a></li>
+                                    <li><a href="https://sarkariprep.in/disclaimer/">डिस्क्लेमर</a></li>
+                                </ul>
+                            </li>
+                            <!-- 6 Drop Down End -->
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <div class="main-data">
+        <div class="app">
+        <center><p>सरकारी नौकरी एवं सरकारी योजना के लिए Sakariprep App डाउनलोड करे।</p>
+        <img src="https://sarkariprep.in/images/sarkariprep-app.jpg" alt="Sarkariprep App" height="400" style="border:3px solid #000; border-radius:10px; box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 20px;"><br>
+	
+        <a href="https://play.google.com/store/apps/details?id=com.app.sarkariprep" target="_blank" class="chip">
+            
+        <img src="https://sarkariprep.in/images/playstore.png" alt="Playstore" width="90" height="90">
+            Download Now
+        </a></center>
+    </div>
+</div>
+<!--Footer Start-->
+<footer class="footer">
+  <div class="footer-center">
+	 <p>© 2022. Sarkariprep | <a href="https://sarkariprep.in/about/" target="_blank" rel="noopener">About</a> | <a href="https://sarkariprep.in/contact/" target="_blank" rel="noopener">Contact</a> | <a href="https://sarkariprep.in/privacy-policy/" target="_blank" rel="noopener">Privacy Policy</a> | <a href="https://sarkariprep.in/disclaimer/" target="_blank" rel="noopener">Disclaimer</a> | <a href="https://sarkariprep.in/sitemap.xml" target="_blank" rel="noopener">Sitemap</a> | Made In India</p>
+	 </div>
+</footer>
+<!--Footer End-->
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "195px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+</script>
+
+</body>
+</html>
